ee07cc734af9aa219aa1649afcbbfc3f97c15b39
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-08-30 02:50:28 +0200

    Added test game_state to demo.
    See issue multicore_engine/multicore_engine#117

--- multicore_engine_demo/src/main.cpp
+++ multicore_engine_demo/src/main.cpp
@@ -5,7 +5,9 @@
  */
 
 #include <mce/core/engine.hpp>
+#include <mce/core/game_state_machine.hpp>
 #include <mce/core/window_system.hpp>
+#include <mce/demo/test_state.hpp>
 #include <mce/graphics/graphics_system.hpp>
 #include <mce/rendering/renderer_system.hpp>
 
@@ -14,4 +16,5 @@ int main() {
 	auto ws = eng.add_system<mce::core::window_system>(9, 9);
 	auto gs = eng.add_system<mce::graphics::graphics_system>(10, 10, *ws);
 	eng.add_system<mce::rendering::renderer_system>(11, 11, *gs);
+	eng.game_state_machine().enter<mce::demo::test_state>();
 }
