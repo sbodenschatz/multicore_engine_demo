92c4eca469fb1c7d657eb2ecf7b92092d3861f2b
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2017-10-24 18:30:09 +0200

    Updated name for input_state to reflect upstream rename.

--- multicore_engine_demo/src/demo/test_state.cpp
+++ multicore_engine_demo/src/demo/test_state.cpp
@@ -7,7 +7,7 @@
 #include <cassert>
 #include <mce/asset/asset_manager.hpp>
 #include <mce/demo/test_state.hpp>
-#include <mce/input/input_state.hpp>
+#include <mce/input/first_person_input_state.hpp>
 #include <mce/input/input_system.hpp>
 #include <mce/rendering/renderer_state.hpp>
 #include <mce/rendering/renderer_system.hpp>
@@ -20,7 +20,7 @@ test_state::test_state(mce::core::engine* engine, mce::core::game_state_machine*
 					   mce::core::game_state* parent_state)
 		: entity_game_state(engine, state_machine, parent_state) {
 	add_system_state<mce::rendering::renderer_state>();
-	add_system_state<mce::input::input_state>();
+	add_system_state<mce::input::first_person_input_state>();
 	add_system_state<mce::simulation::actuator_state>();
 	auto ent_asset = engine->asset_manager().load_asset_sync("entities/demo.etl");
 	entity_manager().load_entities_from_template_lang_file(ent_asset);
