94eb4e5324cebcb393a4591545361c88e091b98f
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2019-11-13 07:13:07 +0100

    Switched to VS 2019.

--- create_windows_build_env.ps1
+++ create_windows_build_env.ps1
@@ -14,5 +14,5 @@ cd ../build.mingw.release
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$((get-item $pwd).parent.FullName)\install.mingw" $src_dir
 mkdir ../build.vs -ea 0
 cd ../build.vs
-cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX="$((get-item $pwd).parent.FullName)\install.vs" $src_dir
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX="$((get-item $pwd).parent.FullName)\install.vs" $src_dir
 cd $src_dir
